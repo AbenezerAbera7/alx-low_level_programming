f45fd40a6d7a39a3bbd8923862d2ea10924912a1
Abenina14 <abenynin2314@gmail.com>
2022-04-07 11:41:27 +0300

    put using "puts" function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle"
  * Return: should end with(return) value 0(zero) if it is correct.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
