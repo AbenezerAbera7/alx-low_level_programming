dc4ee5bfbb1a7c51f652bfad41d0b7129ce53979
Abenina14 <abenynin2314@gmail.com>
2022-04-14 09:42:42 +0300

    Check for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int letter)
+{
+	return (letter >= 'A' && letter <= 'Z');
+}
