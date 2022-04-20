ca9f26ff72f3876a97815580c07ae6502ab173de
Abenina14 <abenynin2314@gmail.com>
2022-04-20 14:21:44 +0300

    Encode string using file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	int i = 0, j = 0;
 	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZ";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
 	while (s[i] != '\0')
 	{
